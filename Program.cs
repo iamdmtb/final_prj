7a8a2acc09e5aedcf58c67ead83aee426994ef43
dbtb <mitbat@inbox.ru>
2023-04-27 15:41:55 +0300

    написал программу в двух вариантах

--- Program.cs
+++ Program.cs
@@ -1,1 +1,65 @@
-﻿
+﻿//РЕШЕНИЕ 1 (с использованием двух массивов)
+Console.Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+ Console.WriteLine($"Введите {i+1}-й элемент: ");
+ string element = Convert.ToString(Console.ReadLine());
+ arrayStrings[i] = element;
+}
+
+string[] arrayFinal = new string[size];
+int len = 3;
+int pos = 0;
+
+for (int j = 0; j < size; j++)
+{
+ if (arrayStrings[j].Length <= len)
+    {
+ arrayFinal[pos] = arrayStrings[j];
+ pos++;
+    }
+}
+
+Console.WriteLine();
+PrintArray(arrayFinal);
+
+// РЕШЕНИЕ 2 (с использованием одного массива)
+/* 
+Console.Write("Сколько элементов Вы хотите ввести? ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+int len = 3;
+int pos = 0;
+
+for (int i = 0; i < size; i++)
+{
+ Console.WriteLine($"Введите {i+1}-й элемент: ");
+ string element = Convert.ToString(Console.ReadLine());
+
+ if (element.Length <= len)
+    {
+ arrayStrings[pos] = element;
+ pos++;
+    }
+}
+
+Console.WriteLine();
+PrintArray(arrayStrings);
+*/
+
+// _____________Блок вспомогательных методов_____________
+//
+//_______________________________________________________
+
+void PrintArray(string[] array)
+{
+ for (int i = 0; i < array.Length; i++)
+    {
+ Console.Write(array[i] + " ");
+    }
+ Console.WriteLine();
+}
